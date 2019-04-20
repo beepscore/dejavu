c0d0d8da54eee3b3e18d38f331a0d7b34348a0bb
beepscore <support@beepscore.com>
2019-04-19 20:18:34 -0700

    Add directory data, move mp3 into it. Ignore data.

--- recognize_audio.py
+++ recognize_audio.py
@@ -18,12 +18,12 @@ if __name__ == '__main__':
     djv = Dejavu(dburl=dburl)
 
     # Fingerprint all the mp3's in the directory we give it
-    djv.fingerprint_directory("mp3", [".mp3"])
+    djv.fingerprint_directory("data/mp3", [".mp3"])
 
     # Recognize audio from a file
     song = djv.recognize(
-        # FileRecognizer, "mp3/Sean-Fournier--Falling-For-You.mp3"
-        FileRecognizer, "mp3/chantix.mp3"
+        # FileRecognizer, "data/mp3/Sean-Fournier--Falling-For-You.mp3"
+        FileRecognizer, "data/mp3/chantix.mp3"
     )
     print("From file we recognized: %s\n" % song)
 
@@ -40,6 +40,6 @@ if __name__ == '__main__':
     # Or use a recognizer without the shortcut, in anyway you would like
     # recognizer = FileRecognizer(djv)
     # song = recognizer.recognize_file(
-    #     "mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3"
+    #     "data/mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3"
     # )
     # print("No shortcut, we recognized: %s\n" % song)
